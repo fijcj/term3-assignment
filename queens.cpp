619d3f8645efd90b38c9799beef5ebd19cd61f6b
Bosco <bosco.chau@education.nsw.gov.au>
2021-08-26 21:40:46 +1000

    changed autos to what they actually are
    Signed-off-by: Bosco <bosco.chau@education.nsw.gov.au>

--- queens.cpp
+++ queens.cpp
@@ -33,8 +33,8 @@ int main() {
     for (size_t i = 0; i != 8; ++i) {
         barr[yS][i] = true;
     }
-    for (const auto &a : barr) {
-        for (const auto b : a) {
+    for (const bool (&a)[8] : barr) {
+        for (const bool b : a) {
             std::cout << b << ' ';
         }
         std::cout << std::endl;
