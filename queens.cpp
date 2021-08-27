6c91e70ae9df775d5dea9501043dd9e55a807d78
fijcj <85040751+fijcj@users.noreply.github.com>
2021-08-27 15:58:01 +1000

    Update queens.cpp

--- queens.cpp
+++ queens.cpp
@@ -8,17 +8,30 @@ int main() {
     std::cin >> x >> y;
     bool barr[8][8] = {false};
     short yS = y-1, xS = x-1;
+    
     for (size_t i = xS; i != 8 && yS != -1; ++i, --yS) barr[yS][i] = true;
+    
     resetValues(yS, xS, y-1, x-1);
+    
     for (size_t i = xS; i != -1 && yS != 8; --i, ++yS) barr[yS][i] = true;
+    
     resetValues(yS, xS, y-1, x-1);
+    
     for (size_t i = yS; i != -1 && xS != -1; --i, --xS) barr[i][xS] = true;
+    
     resetValues(yS, xS, y-1, x-1);
+    
     for (size_t i = yS; i != 8 && xS != 8; ++i, ++xS) barr[i][xS] = true;
+    
     resetValues(yS, xS, y-1, x-1);
+    
     for (size_t i = 0; i != 8; ++i) barr[i][xS] = true;
+    
     resetValues(yS, xS, y-1, x-1);
+    
     for (size_t i = 0; i != 8; ++i) barr[yS][i] = true;
+    
+    
     
     for (const bool (&a)[8] : barr) {
         for (const bool b : a) {
